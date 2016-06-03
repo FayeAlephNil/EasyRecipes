4f8b4184586976355e53389a1c60756ca5da24e7
Striking <Strikingwolf2012@gmail.com>
2016-06-02 23:27:25 -0500

    Make CrossRecipes more generic with CenteredRecipes

---
+++ src/main/java/CenteredRecipe.java
@@ -0,0 +1,43 @@
+import net.minecraft.item.ItemStack;
+
+public class CenteredRecipe extends SimpleRecipe {
+	public CenteredRecipe(ItemStack result, Object center, Object outer, Object inner) {
+		this(result, convert(center), convert(outer), convert(inner));
+	}
+
+	public CenteredRecipe(ItemStack result, IRecipePart center, IRecipePart outer, IRecipePart inner) {
+		super(result, new CenteredPart(center, outer, inner));
+	}
+
+	public static CenteredRecipe cross(ItemStack result, Object crosser, Object bounder) {
+		return new CenteredRecipe(result, crosser, crosser, bounder);
+	}
+
+	public static CenteredRecipe plus(ItemStack result, Object plus, Object bounder) {
+		return new CenteredRecipe(result, plus, bounder, plus);
+	}
+
+	private static class CenteredPart implements IRecipePart {
+
+		private final IRecipePart center;
+		private final IRecipePart outer;
+		private final IRecipePart inner;
+
+		public CenteredPart(IRecipePart center, IRecipePart outer, IRecipePart inner) {
+			this.center = center;
+			this.outer = outer;
+			this.inner = inner;
+		}
+
+		@Override
+		public Object get(int position) {
+			if (position == 4) {
+				return center;
+			} else if (position % 2 == 0) {
+				return outer;
+			} else {
+				return inner;
+			}
+		}
+	}
+}
